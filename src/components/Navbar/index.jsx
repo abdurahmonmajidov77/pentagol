79418b74e91deb34b687bbc934994fc0b99c99cf
Abdurahmon Majidov <109199563+abdurahmonmajidov77@users.noreply.github.com>
2023-04-29 23:30:00 +0500

    hi

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,11 @@
+import './style.css';
+
+function Navbar() {
+  return (
+    <div className="Navbar">
+        
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import Navbar from '../../components/Navbar';
+import './style.css';
+
+function Home() {
+  return (
+    <div className="Home">
+        <Navbar/>
+    </div>
+  );
+}
+
+export default Home;
